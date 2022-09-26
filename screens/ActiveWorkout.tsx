79d9681d94839d57427235e0d369039ddba04a18
Ahmed McGarry <atomcgarry@gmail.com>
2022-09-26 16:58:28 +0100

    modal and zustand

--- screens/ActiveWorkout.tsx
+++ screens/ActiveWorkout.tsx
@@ -6,12 +6,57 @@ import {
   Heading,
   Text,
   Pressable,
+  Modal,
+  FlatList,
+  Input,
 } from "native-base";
 import { useState } from "react";
+import ExerciseCard from "../components/layout/ExerciseCard";
 import Timer from "../components/Timer";
+import data from "../data";
 
 const ActiveWorkout = () => {
   const [workoutTitle, setWorkoutTitle] = useState("Quick Workout");
+  const [addLiftIsHidden, setAddLiftIsHidden] = useState(true);
+
+  const showModal = () => setAddLiftIsHidden(false);
+
+  const renderModal = () => (
+    <Modal isOpen={!addLiftIsHidden} onClose={showModal} size={"lg"}>
+      <Modal.Content>
+        <Modal.CloseButton />
+        <Modal.Header>Add a Lift</Modal.Header>
+        <Modal.Body>
+          <Box
+            flexDirection={"row"}
+            alignItems={"center"}
+            borderWidth={1}
+            borderColor={"whitesmoke"}
+          >
+            <FontAwesome
+              name="search"
+              size={20}
+              color={"black"}
+              style={{ marginRight: 2 }}
+            />
+            <Input placeholder="Search for a lift" borderWidth={0} />
+          </Box>
+        </Modal.Body>
+        <Modal.Body>
+          {/* 
+            ON CLICK THE EXERCISE CARD GETS ADDED TO THE ACTIVE WORKOUT 
+            CARD NEEDS TO ACCEPT PROP THAT TELLS IT TO ADD TO AN ACTIVE WORKOUT
+          */}
+          <FlatList
+            data={Object.values(data)}
+            renderItem={({ item }) => <ExerciseCard item={{ ...item }} />}
+            keyExtractor={({ id }) => id}
+          />
+        </Modal.Body>
+      </Modal.Content>
+    </Modal>
+  );
+
   return (
     <View padding={3} flex={1} backgroundColor={"white"}>
       <Box flex={1}>
@@ -26,7 +71,12 @@ const ActiveWorkout = () => {
         <Timer />
       </Box>
       <Box marginBottom={2}>
-        <Button marginY={1} backgroundColor="info.400" height={10}>
+        <Button
+          onPress={showModal}
+          marginY={1}
+          backgroundColor="info.400"
+          height={10}
+        >
           <Text color="white" fontWeight="semibold">
             Add A Lift
           </Text>
@@ -37,6 +87,7 @@ const ActiveWorkout = () => {
           </Text>
         </Button>
       </Box>
+      {renderModal()}
     </View>
   );
 };
