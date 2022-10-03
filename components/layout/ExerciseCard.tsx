c40252bf77d587a3ea1f2f77e1fdac6f1040baed
Ahmed McGarry <atomcgarry@gmail.com>
2022-10-03 16:56:09 +0100

    refactor card

--- components/layout/ExerciseCard.tsx
+++ components/layout/ExerciseCard.tsx
@@ -10,28 +10,30 @@ interface Props {
   description: string;
   image: string;
   id: number;
+  isPressable?: boolean;
 }
 
-const ExerciseCard = (exercise: Props) => {
-  const { exercise_name, targets } = exercise;
+const ExerciseCard = ({
+  exercise_name,
+  targets,
+  isPressable = false,
+}: Props) => {
   const [isActive, setIsActive] = useState(false);
 
   const backgroundColor = isActive ? "info.50" : "white";
+  const handlePress = () => setIsActive((state) => !state);
 
-  return (
-    <Box padding={2} borderColor={"gray.200"} backgroundColor={backgroundColor}>
-      <Pressable
-        flexDirection={"row"}
-        alignItems="center"
-        onPress={() => setIsActive((state) => !state)}
-      >
-        <Avatar backgroundColor={generateColor()} marginRight={2}>
-          <Text>{getExerciseInitials(exercise_name)}</Text>
-        </Avatar>
-        <Box flex={1}>
-          <Text fontWeight={"semibold"}>{exercise_name}</Text>
-          <Text color="text.900">{targets}</Text>
-        </Box>
+  const renderAvatar = () => {
+    return (
+      <Avatar backgroundColor={generateColor()} marginRight={2}>
+        <Text>{getExerciseInitials(exercise_name)}</Text>
+      </Avatar>
+    );
+  };
+
+  const renderCheckbox = () => {
+    if (isPressable) {
+      return (
         <Box>
           <Checkbox
             value="isActive"
@@ -43,6 +45,31 @@ const ExerciseCard = (exercise: Props) => {
             _checked={{ backgroundColor: "info.200" }}
           />
         </Box>
+      );
+    }
+
+    return null;
+  };
+
+  const renderBody = () => {
+    return (
+      <Box flex={1}>
+        <Text fontWeight={"semibold"}>{exercise_name}</Text>
+        <Text color="text.900">{targets}</Text>
+      </Box>
+    );
+  };
+
+  return (
+    <Box padding={2} borderColor={"gray.200"} backgroundColor={backgroundColor}>
+      <Pressable
+        flexDirection={"row"}
+        alignItems="center"
+        onPress={isPressable ? handlePress : null}
+      >
+        {renderAvatar()}
+        {renderBody()}
+        {renderCheckbox()}
       </Pressable>
     </Box>
   );
