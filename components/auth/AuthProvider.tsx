b2f06c86b911deb849e28f8fa51ecd0d984bc8ae
Ahmed McGarry <atomcgarry@gmail.com>
2022-11-10 12:51:12 +0000

    clean up auth provider

--- components/auth/AuthProvider.tsx
+++ components/auth/AuthProvider.tsx
@@ -7,19 +7,18 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const isLoggedIn = useAppSelector((state) => state.userSlice.user.isLoggedIn);
-  const isLoading = useAppSelector((state) => state.userSlice.status);
+  const isLoading =
+    useAppSelector((state) => state.userSlice.status) === "pending";
 
-  if (isLoading === "pending") {
+  if (isLoading) {
     return (
       <View h={"full"} justifyContent="center" alignItems={"center"}>
         <Spinner />
       </View>
     );
   }
-  if (isLoggedIn) return <>{children}</>;
-  if (!isLoggedIn) return <Auth />;
 
-  return null;
+  return <>{isLoggedIn ? children : <Auth />}</>;
 };
 
 export default AuthProvider;
