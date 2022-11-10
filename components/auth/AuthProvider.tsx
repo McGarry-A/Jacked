fbc682082a130cad369f1619db462493ec14d669
Ahmed McGarry <atomcgarry@gmail.com>
2022-11-10 12:42:36 +0000

    move auth component to auth file

--- components/auth/AuthProvider.tsx
+++ components/auth/AuthProvider.tsx
@@ -1,7 +1,7 @@
 import { Spinner, View } from "native-base";
 import React from "react";
 import { useAppSelector } from "../../store";
-import Auth from "../Auth";
+import Auth from "./Auth";
 
 const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
