21d54a5fe0392ee6de109899be3c52aee20a0f06
Ahmed <atomcgarry@gmail.com>
2022-11-13 21:57:36 +0000

    clean up template component on history page

--- components/layout/Template.tsx
+++ components/layout/Template.tsx
@@ -1,5 +1,5 @@
 import { FontAwesome } from "@expo/vector-icons";
-import { faClock } from "@fortawesome/free-regular-svg-icons/faClock"
+import { faClock } from "@fortawesome/free-regular-svg-icons/faClock";
 import { Badge, Box, HStack, Skeleton, Text, VStack } from "native-base";
 import { useAppSelector } from "../../store";
 import React, { memo } from "react";
@@ -26,7 +26,7 @@ const Template = ({ width, workoutName, date, lifts }: TemplateProps) => {
 
   const renderHeader = () => {
     return (
-      <Box flexDirection={"row"} alignItems={"center"}>
+      <HStack>
         <Text
           flex={1}
           fontSize={textSize}
@@ -45,7 +45,7 @@ const Template = ({ width, workoutName, date, lifts }: TemplateProps) => {
         >
           <FontAwesome name="ellipsis-h" size={15} color={"skyblue"} />
         </Box>
-      </Box>
+      </HStack>
     );
   };
 
@@ -68,10 +68,7 @@ const Template = ({ width, workoutName, date, lifts }: TemplateProps) => {
 
   const renderDaysAgo = () => {
     return (
-      <Box
-        flexDirection={"row"}
-        alignItems={"center"}
-      >
+      <Box flexDirection={"row"} alignItems={"center"}>
         <FontAwesomeIcon icon={faClock} size={13} color="gray" />
         <Text fontSize={"xs"} opacity={50} ml={1}>
           {getDaysAgo(date)}
@@ -81,13 +78,25 @@ const Template = ({ width, workoutName, date, lifts }: TemplateProps) => {
   };
 
   const renderLifts = () => {
-    if (Object.keys(lifts).length === 0) return <Text>No Lifts</Text>;
+    const liftsLength = Object.keys(lifts).length;
+    const showComma = (index: number) =>
+      liftsLength === 1 || index + 1 === liftsLength ? " " : ", ";
+
+    if (liftsLength === 0)
+      return (
+        <Text fontSize={"sm"} color={"text.600"}>
+          No Lifts
+        </Text>
+      );
 
     return (
       <Box marginY={2}>
         <Text fontSize={"sm"} color={"text.600"}>
           {Object.values(lifts).map((el, index) => (
-            <React.Fragment key={index}>{el.exercise_name}, </React.Fragment>
+            <React.Fragment key={index}>
+              {el.exercise_name}
+              {showComma(index)}
+            </React.Fragment>
           ))}
         </Text>
       </Box>
@@ -95,6 +104,7 @@ const Template = ({ width, workoutName, date, lifts }: TemplateProps) => {
   };
 
   const renderOnSwipeRight = () => {
+    // NOTE: delete should be shown on the popup instead
     return (
       <Box backgroundColor={"red.500"} w={"full"} justifyContent={"center"}>
         <Text fontWeight={"700"} textAlign={"center"} color={"white"}>
