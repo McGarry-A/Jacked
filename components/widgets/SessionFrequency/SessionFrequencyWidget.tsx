c241613743f5db6458479c1e71dd7814c68385d3
Ahmed McGarry <atomcgarry@gmail.com>
2023-01-30 16:10:48 +0000

    fixing bar chart styles and segment size

--- components/widgets/SessionFrequency/SessionFrequencyWidget.tsx
+++ components/widgets/SessionFrequency/SessionFrequencyWidget.tsx
@@ -5,25 +5,40 @@ import useSessionFrequency from "../../../hooks/useSessionFrequency";
 const SessionFrequencyWidget: React.FC = () => {
   const { labels, values, isLoading } = useSessionFrequency();
 
-  const segmentsCount = Math.max(...values);
+  const maxInValues = Math.max(...values);
+  const segmentCount = maxInValues < 4 ? maxInValues : 4;
 
   const data = {
     labels: labels,
-    datasets: [{ data: values }],
+    datasets: [
+      {
+        data: values,
+        colors: [
+          (opacity = 1) => `#702963`,
+          (opacity = 1) => `#702963`,
+          (opacity = 1) => `#702963`,
+          (opacity = 1) => `#702963`,
+          (opacity = 1) => `#702963`,
+          (opacity = 1) => `#702963`,
+          (opacity = 1) => `#702963`,
+        ],
+      },
+    ],
   };
 
   return !isLoading ? (
     <BarChart
+      yAxisLabel=""
+      yAxisSuffix=""
       data={data}
       width={SCREEN_WIDTH - 49}
       height={160}
-      yAxisLabel=""
       chartConfig={CONFIG}
-      yAxisSuffix=""
       showBarTops={false}
       withInnerLines={true}
-      fromZero
-      segments={4}
+      segments={segmentCount}
+      withCustomBarColorFromData={true}
+      yAxisInterval={1}
       style={{
         marginVertical: 8,
         borderRadius: 16,
--- components/widgets/SessionFrequency/config.ts
+++ components/widgets/SessionFrequency/config.ts
@@ -3,16 +3,10 @@ import { Dimensions } from "react-native";
 const CONFIG = {
   backgroundGradientFrom: "#FFFF",
   backgroundGradientFromOpacity: 0,
-  backgroundGradientTo: "#FFFF",
-  backgroundGradientToOpacity: 0,
   color: () => "#808080",
   barPercentage: 0.6,
   barRadius: 3,
   decimalPlaces: 0,
-  fillShadowGradientFrom: "#702963",
-  fillShadowGradientFromOpacity: 0.8,
-  fillShadowGradientToOpacity: 1,
-  fillShadowGradientTo: "#7851a9",
   propsForBackgroundLines: {
     strokeWidth: "0.3",
     strokeLinecap: "round",
