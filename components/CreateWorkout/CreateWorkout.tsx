48a7657818277ce308c21f0d104b331a693260bd
Ahmed McGarry <atomcgarry@gmail.com>
2023-02-13 11:00:33 +0000

    J-57 clean up component

--- components/CreateWorkout/CreateWorkout.tsx
+++ components/CreateWorkout/CreateWorkout.tsx
@@ -10,40 +10,21 @@ import {
   Button,
   HStack,
 } from "native-base";
-import { useEffect, useRef } from "react";
 import { useState } from "react";
 import Timer from "../../components/utils/Timer";
 import { useAppDispatch, useAppSelector } from "../../store";
-import {
-  setWorkoutTitle as setWorkoutName,
-  saveWorkout,
-} from "../../store/currentWorkoutSlice";
+import { saveWorkout, setWorkoutTitle } from "../../store/currentWorkoutSlice";
 import Lift from "../../components/layout/Lift";
 import { useNavigation } from "@react-navigation/native";
 
-const CreateWorkout = ({ route: { params } }: any) => {
-  const { workoutTitle } = useAppSelector((state) => state.currentWorkoutSlice);
-
-  const [displayedWorkoutName, setDisplayedWorkoutName] = useState(
-    workoutTitle ? workoutTitle : "Quick Workout"
-  );
-
-  const workoutTitleRef = useRef<HTMLInputElement>(null);
+const CreateWorkout = () => {
+  const [title, setTitle] = useState<string>("");
   const state = useAppSelector((state) => state.currentWorkoutSlice);
   const dispatch = useAppDispatch();
   const { navigate } = useNavigation();
 
-  useEffect(() => {
-    if (params) {
-      const { title } = params;
-
-      setDisplayedWorkoutName(title);
-      dispatch(setWorkoutName(title));
-    }
-  }, []);
-
   const handleEndWorkout = () => {
-    dispatch(setWorkoutName(displayedWorkoutName));
+    dispatch(setWorkoutTitle(title));
     dispatch(saveWorkout());
     // navigate("Profile");
   };
@@ -54,14 +35,13 @@ const CreateWorkout = ({ route: { params } }: any) => {
         flexDirection={"row"}
         alignItems="center"
         justifyContent={"space-between"}
-        value={displayedWorkoutName}
-        onChangeText={(text) => setDisplayedWorkoutName(text)}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
         borderWidth={0}
         fontWeight={700}
         color={"coolGray.700"}
         _focus={{ backgroundColor: "white" }}
         fontSize={"2xl"}
-        ref={workoutTitleRef}
         editable
         rightElement={renderEndWorkout()}
       />
@@ -107,12 +87,12 @@ const CreateWorkout = ({ route: { params } }: any) => {
 
   const renderButtons = () => {
     return (
-      <Box marginBottom={2}>
+      <Box padding={3}>
         <Pressable
           onPress={() => navigate("AddExercises")}
           marginY={1}
           backgroundColor="info.400"
-          height={8}
+          height={9}
           justifyContent={"center"}
         >
           <Text color="white" fontWeight={700} textAlign={"center"}>
@@ -123,28 +103,30 @@ const CreateWorkout = ({ route: { params } }: any) => {
     );
   };
 
+  const renderNoLifts = () => {
+    return (
+      <Box flexGrow={1} justifyContent={"center"} alignItems="center">
+        <Image
+          source={require("../../images/undraw_Powerful_re_frhr.png")}
+          alt={"Personal Trainer Training Client"}
+          size={"2xl"}
+        />
+        <Text
+          color={"text.700"}
+          opacity={50}
+          fontWeight="semibold"
+          textTransform="capitalize"
+        >
+          Add some lifts to start your workout!
+        </Text>
+      </Box>
+    );
+  };
+
   const renderLifts = () => {
     const { exercises } = state;
 
-    if (Object.keys(exercises).length < 1) {
-      return (
-        <Box flexGrow={1} justifyContent={"center"} alignItems="center">
-          <Image
-            source={require("../../images/undraw_Powerful_re_frhr.png")}
-            alt={"Personal Trainer Training Client"}
-            size={"2xl"}
-          />
-          <Text
-            color={"text.700"}
-            opacity={50}
-            fontWeight="semibold"
-            textTransform="capitalize"
-          >
-            Add some lifts to start your workout!
-          </Text>
-        </Box>
-      );
-    }
+    if (Object.keys(exercises).length < 1) return renderNoLifts();
 
     return (
       <VStack flex={1} px={2} space={1}>
@@ -172,7 +154,7 @@ const CreateWorkout = ({ route: { params } }: any) => {
       {renderHeading()}
       {renderTimer()}
       {renderLifts()}
-      <Box padding={3}>{renderButtons()}</Box>
+      {renderButtons()}
     </View>
   );
 };
