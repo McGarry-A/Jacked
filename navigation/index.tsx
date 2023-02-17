0b8a4bb102a13d9965673a09876db41a8526adad
Ahmed <atomcgarry@gmail.com>
2023-02-17 15:51:01 +0000

    -

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -31,7 +31,7 @@ import AddExercises from "../screens/modals/AddExercises";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useAppDispatch, useAppSelector } from "../store";
 import Start from "../screens/root/Start";
-import ColorThemeSwitch from "../components/Layout/ColorThemeSwitch";
+import ColorThemeSwitch from "../components/Utils/ColorThemeSwitch";
 import Auth from "../components/Auth/Auth";
 import Welcome from "../components/Auth/Welcome";
 import Header from "../components/Layout/ScreenHeader";
