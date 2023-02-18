346b48f70a8f7eb664a47b25fdc03af2b2bce5cb
Ahmed <atomcgarry@gmail.com>
2023-02-18 21:19:07 +0000

    changed bar to a flat colour

--- components/widgets/SessionFrequency/config.ts
+++ components/widgets/SessionFrequency/config.ts
@@ -1,8 +1,12 @@
 import { Dimensions } from "react-native";
 
-const CONFIG = {
-  backgroundGradientFrom: "#FFFF",
+const BAR_CONFIG = {
   backgroundGradientFromOpacity: 0,
+  backgroundGradientToOpacity: 0,
+  fillShadowGradientFromOpacity: 0,
+  fillShadowGradientToOpacity: 0,
+  fillShadowGradientFrom: '#7851a9',
+  fillShadowGradientTo: '#7851a9',
   color: () => "#808080",
   barPercentage: 0.6,
   barRadius: 3,
@@ -13,11 +17,8 @@ const CONFIG = {
     strokeDasharray: "0, 0",
     x1: 20,
   },
-  propsForLabels: {
-    // fontFamily: "sans-serif",
-  },
 };
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
-export { CONFIG, SCREEN_WIDTH };
+export { BAR_CONFIG, SCREEN_WIDTH };
