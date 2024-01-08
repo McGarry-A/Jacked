60cbc20b13819f0d6b83e539326a8dbc6e1a4e7a
Ahmed McGarry <130065744+AhmedMcgarryUZ@users.noreply.github.com>
2024-01-08 21:30:45 +0000

    Added path alias

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,46 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-    }]]
-  }
-}
+// eslint-disable-next-line no-undef
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            "react-native-reanimated/plugin",
+            [
+                "module-resolver",
+                {
+                    extensions: [
+                        ".ios.js",
+                        ".android.js",
+                        ".ios.jsx",
+                        ".android.jsx",
+                        ".js",
+                        ".jsx",
+                        ".json",
+                        ".ts",
+                        ".tsx",
+                    ],
+                    root: ["."],
+                    alias: {
+                        "@Components": "./components",
+                        "@Supabase": "./supabase",
+                        "@Store": "./store",
+                        "@Navigation": "./navigation",
+                        "@Hooks": "./hooks",
+                        "@Constants": "./constants",
+                    },
+                },
+            ],
+            [
+                "module:react-native-dotenv",
+                {
+                    moduleName: "@env",
+                    path: ".env",
+                    blacklist: null,
+                    whitelist: null,
+                    safe: false,
+                    allowUndefined: true,
+                },
+            ],
+        ],
+    };
+};
